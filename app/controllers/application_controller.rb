c07952734296e55aa4ead25d2fab2716b2a871ae
Joshua Hill <josh@jlhworkshop.com>
2019-08-28 16:47:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    @articles = Article.new
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    
+  end
+  
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+  
+  post '/articles' do
+    Article.new(params)
+    erb :show
+  end
+  
+  patch '/articles/:id' do
+    
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
-#Placeholder for a model
-
 class Article < ActiveRecord::Base
+  
+  # attr_accessor :title, :content
+  
+  # @articles = []
+  
+  # def initialize(title, content)
+  #   self.title = title
+  #   self.content = content
+  #   @articles << self
+  # end
+  
+  # def self.all
+  #   return @articles
+  # end
+  
 end
